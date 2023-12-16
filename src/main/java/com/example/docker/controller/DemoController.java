3375346d4cff11ac2577b29473f8ed3afbc1fdc8
sauravsahoosks <saurav101sahoo.sks@yahoo.com>
2023-12-16 12:29:46 +0530

    Controller added

---
+++ src/main/java/com/example/docker/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.example.docker.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+
+@Validated
+@RestController
+@RequestMapping("demo-controller")
+public class DemoController {
+
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from secured endpoint");
+    }
+}
